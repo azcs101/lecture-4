a9d3625893b1568b6ed302ba1aef37c960032165
Gasim Gasimzada <gasim@appristas.io>
2018-04-27 04:39:15 +0400

    Initial commit

---
+++ 7-pointers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+	int a = 20;
+	int b = 10;
+	int c = 0;
+	float d = 7.4;
+	char e = 'c'
+	int f = -23;
+
+	return 0;
+}
---
+++ 8-arrays.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	int numbers[10] = { 1, 2, 3, 5, 6, 7, 8, 9, 4, 15 };
+
+	return 0;
+}
